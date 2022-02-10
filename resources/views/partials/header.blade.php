f380648cb0ed312e3a56a7d8c345abef8c6f67ef
tgiannotta <t.giannotta92@gmail.com>
2022-02-10 10:38:18 +0100

    end header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,55 @@
 <header>
-    <h2>Prova Header</h2>
+    
+        <div class="sites-container">
+                <div class="sites">
+                <h3>Prova siti</h3>
+            </div>
+
+        </div>
+        
+        <div class="nav">
+           
+            <ul>
+                <li class="logo">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="Logo-DC">
+                    
+                </li>
+                <li>
+                    <a href="#"> Characters </a>
+                </li>
+                <li>
+                    <a href="#"> Comics </a>
+                </li>
+                <li>
+                    <a href="#"> Movies </a>
+                </li>
+                <li>
+                    <a href="#"> TV </a>
+                </li>
+                <li>
+                    <a href="#"> Games </a>
+                </li>
+                <li>
+                    <a href="#"> collectables </a>
+                </li>
+                <li>
+                    <a href="#"> Videos </a>
+                </li>
+                <li>
+                    <a href="#"> Fans </a>
+                </li>
+                <li>
+                    <a href="#"> news </a>
+                </li>
+                <li>
+                    <a href="#"> shop </a>
+                </li>
+                <li>
+                    <button>Search</button>
+                </li>
+            </ul>
+        </div>
+        
+    
+    
 </header>
